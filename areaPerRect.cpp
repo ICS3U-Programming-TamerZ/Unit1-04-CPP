30eee3b2c1402acc2da4e4d11dd05af66468bef3
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-09-16 18:02:34 +0000

    Added areaPerRect.cpp basicMath.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Tamer Zreg All rights reservedcpplint areaPerRect.cpp
+// Created by: Tamer Zreg
+// Created on: Sep. 16, 2022
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 10cm X 6cm.
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 10cm and width 6cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 10 * 6 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (10 + 6) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+//// Copyright (c) 2022 Tamer Zreg All rights reservedcpplint basicMath.cpp
+// Created by: Tamer Zreg
+// Created on: Sep. 16, 2022
+// This program does basic math.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+    std::cout << "10 / 2 = " << 10 / 2 << std::endl;
+    std::cout << "8 - 4 / 2 = " << 8 - 4 / 2 << std::endl;
+    std::cout << "2^3 = " << pow(2, 3) << std::endl;
+    std::cout << "1 / 3 = " << float(1) / float(3) << std::endl;
+}
